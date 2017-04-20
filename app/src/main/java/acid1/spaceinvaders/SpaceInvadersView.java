2ca22134288217467f6b0c9c475f85178bcbbaef
Alexandru Matei Prodrom <prodromalex@yahoo.com>
2017-04-20 21:19:05 +0200

    Main entry point to the app is done. Resolution is taken and passed to the SpaceInvaderView and pause and resume are set

---
+++ app/src/main/java/acid1/spaceinvaders/SpaceInvadersView.java
@@ -0,0 +1,26 @@
+package acid1.spaceinvaders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceView;
+
+/**
+ * Created by prodromalex on 4/20/2017.
+ */
+
+public class SpaceInvadersView extends SurfaceView implements Runnable{
+    public SpaceInvadersView(MainActivity mainActivity, int x, int y) {
+        super();
+
+    }
+
+    public void resume() {
+    }
+
+    public void pause() {
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
