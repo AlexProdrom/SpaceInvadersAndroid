6f197da6a40d2c37799ef31e64d95a82cc521bfa
Alexandru Matei Prodrom <prodromalex@yahoo.com>
2017-04-21 17:15:45 +0200

    fields and constructor in spaceinvaderview

--- app/src/main/java/acid1/spaceinvaders/SpaceInvadersView.java
+++ app/src/main/java/acid1/spaceinvaders/SpaceInvadersView.java
@@ -1,15 +1,166 @@
 package acid1.spaceinvaders;
 
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.io.IOException;
+
 
 /**
  * Created by prodromalex on 4/20/2017.
  */
 
 public class SpaceInvadersView extends SurfaceView implements Runnable{
-    public SpaceInvadersView(MainActivity mainActivity, int x, int y) {
-        super();
+
+    Context context;
+
+    // This is our thread
+    private Thread gameThread = null;
+
+    // Our SurfaceHolder to lock the surface before we draw our graphics
+    private SurfaceHolder ourHolder;
+
+    // A boolean which we will set and unset
+    // when the game is running- or not.
+    private volatile boolean playing;
+
+    // Game is paused at the start
+    private boolean paused = true;
+
+    // A Canvas and a Paint object
+    private Canvas canvas;
+    private Paint paint;
+
+    // This variable tracks the game frame rate
+    private long fps;
+
+    // This is used to help calculate the fps
+    private long timeThisFrame;
+
+    // The size of the screen in pixels
+    private int screenX;
+    private int screenY;
+
+    // The players ship
+    private PlayerShip playerShip;
+
+    // The player's bullet
+    private Bullet bullet;
+
+    // The invaders bullets
+    private Bullet[] invadersBullets = new Bullet[200];
+    private int nextBullet;
+    private int maxInvaderBullets = 10;
+
+    // Up to 60 invaders
+    Invader[] invaders = new Invader[60];
+    int numInvaders = 0;
+
+    // The player's shelters are built from bricks
+    private DefenseBrick[] bricks = new DefenseBrick[400];
+    private int numBricks;
+
+    // For sound FX
+    private SoundPool soundPool;
+    private int playerExplodeID = -1;
+    private int invaderExplodeID = -1;
+    private int shootID = -1;
+    private int damageShelterID = -1;
+    private int uhID = -1;
+    private int ohID = -1;
+
+    // The score
+    int score = 0;
+
+    // Lives
+    private int lives = 3;
+
+    // How menacing should the sound be?
+    private long menaceInterval = 1000;
+    // Which menace sound should play next
+    private boolean uhOrOh;
+    // When did we last play a menacing sound
+    private long lastMenaceTime = System.currentTimeMillis();
+
+    public SpaceInvadersView(Context context, int x, int y) {
+        // The next line of code asks the
+        // SurfaceView class to set up our object.
+        super(context);
+
+        // Make a globally available copy of the context so we can use it in another method
+        this.context = context;
+
+        // Initialize ourHolder and paint objects
+        this.ourHolder = getHolder();
+        this.paint = new Paint();
+
+        this.screenX = x;
+        this.screenY = y;
+
+        // This SoundPool is deprecated but don't worry
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC,0);
+
+        try{
+            // Create objects of the 2 required classes
+
+            //Provides access to an application's raw asset files
+            AssetManager assetManager = context.getAssets();
+
+            //File descriptor of an entry in the AssetManager.
+            //This provides your own opened FileDescriptor that can be used to read the data
+            AssetFileDescriptor descriptor;
+
+            // Load our fx in memory ready for use
+            descriptor = assetManager.openFd("shoot.ogg");
+            shootID = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("invaderexplode.ogg");
+            invaderExplodeID = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("damageshelter.ogg");
+            damageShelterID = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("playerexplode.ogg");
+            playerExplodeID = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("damageshelter.ogg");
+            damageShelterID = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("uh.ogg");
+            uhID = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("oh.ogg");
+            ohID = soundPool.load(descriptor, 0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("error", "failed to load sound files");
+        }
+        prepareLevel();
+    }
+
+    private void prepareLevel(){
+
+        // Here we will initialize all the game objects
+
+        // Make a new player space ship
+
+        // Prepare the players bullet
+
+        // Initialize the invadersBullets array
+
+        // Build an army of invaders
+
+        // Build the shelters
 
     }
 
