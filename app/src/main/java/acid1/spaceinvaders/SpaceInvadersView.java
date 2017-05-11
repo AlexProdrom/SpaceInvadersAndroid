adbc6925d4ce77de259c20f293ffc2d677300fb9
Alexandru Matei Prodrom <prodromalex@yahoo.com>
2017-05-11 22:15:32 +0200

    Defense brick

--- app/src/main/java/acid1/spaceinvaders/SpaceInvadersView.java
+++ app/src/main/java/acid1/spaceinvaders/SpaceInvadersView.java
@@ -185,8 +185,17 @@ public class SpaceInvadersView extends SurfaceView implements Runnable{
                 numInvaders ++;
             }
         }
-        // Build the shelters
 
+        // Build the shelters
+        numBricks = 0;
+        for(int shelterNumber = 0; shelterNumber < 4; shelterNumber++){
+            for(int column = 0; column < 10; column ++ ) {
+                for (int row = 0; row < 5; row++) {
+                    bricks[numBricks] = new DefenseBrick(row, column, shelterNumber, screenX, screenY);
+                    numBricks++;
+                }
+            }
+        }
     }
 
     @Override
@@ -361,7 +370,11 @@ public class SpaceInvadersView extends SurfaceView implements Runnable{
                 }
             }
             // Draw the bricks if visible
-
+            for(int i = 0; i < numBricks; i++){
+                if(bricks[i].getVisibility()) {
+                    canvas.drawRect(bricks[i].getRect(), paint);
+                }
+            }
             // Draw the players bullet if active
             if(bullet.getStatus()){
                 canvas.drawRect(bullet.getRect(), paint);
